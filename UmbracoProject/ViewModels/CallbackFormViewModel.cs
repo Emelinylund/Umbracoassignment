cb6e7a1cdc22a264a39c950cbd4db9178a486f95
Emeli Nylund <emsnylund@hotmail.com>
2025-10-30 11:13:47 +0100

    usync add

---
+++ UmbracoProject/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Umbraco.Cms.Core.Models.Membership;
+
+namespace UmbracoProject.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required.")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Phone is required.")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select an option.")]
+    
+    public string SelectedOption { get; set; } = null!;
+}
